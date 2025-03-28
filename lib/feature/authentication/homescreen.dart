3cdfc46765de6c6c55ef166971722b62f2c03040
ayshathahapk <ayshathanhapk12@gmail.com>
2025-03-28 16:25:58 +0530

    push

--- lib/feature/authentication/homescreen.dart
+++ lib/feature/authentication/homescreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../quizzsection/quizpage.dart';
 
 class AuthScreen extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-import 'feature/authentication/homescreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'feature/authentication/homescreen.dart';
 import 'feature/authentication/signout.dart';
 import 'feature/quizzsection/quizpage.dart';
+// import 'firebase_options.dart'; //
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,9 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home:  AuthScreen(),
-      home:  AuthScreen(),
+      home: AuthScreen(),
     );
   }
 }
-
